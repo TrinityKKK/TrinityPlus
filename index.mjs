265256f444b8bf55bba27910cd24d742f34a10bb
0xkkl <kaydan@leozc.com>
2023-05-18 06:50:04 -0700

    fixed uv path

--- index.mjs
+++ index.mjs
@@ -2,7 +2,6 @@ import createBareServer from "@tomphttp/bare-server-node";
 import { fileURLToPath } from "node:url";  
 import http from 'http';
 import fs from 'fs';
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 const bare = createBareServer("/bare/");
 const app = connect();
 import serveStatic from "serve-static";
